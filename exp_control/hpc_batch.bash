d3c59ce7bb7cb0a8544dac894fa8caf1129faaad
Hunter <brownhun@oregonstate.edu>
2025-03-28 13:54:34 -0700

    Ensured random seeding every launch

--- exp_control/hpc_batch.bash
+++ exp_control/hpc_batch.bash
@@ -3,7 +3,7 @@
 #SBATCH -J LR001			    # name of job
 #SBATCH -A virl-grp	            # name of my sponsored account, e.g. class or research group, NOT ONID!
 ##SBATCH -p gpu,eecs2,tiamat,dgxh,dgx2,ampere		# name of partition or queue
-#SBATCH -p eecs2,tiamat,gpu,dgx2
+#SBATCH -p eecs,eecs2,tiamat,gpu,dgx2
 #SBATCH --time=4-23:00:00        # time limit on job: 2 days, 12 hours, 30 minutes (default 12 hours)
 ##SBATCH -N 1                   # number of nodes (default 1)
 #SBATCH --gres=gpu:1            # number of GPUs to request (default 0)
