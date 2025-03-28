f11b7ecc5cda0be9269095e4d7869f5c6fce9d1e
Hunter <brownhun@oregonstate.edu>
2025-03-28 13:19:35 -0700

    Ensured random seeding every launch

--- exp_control/sbatch_exp_launcher.bash
+++ exp_control/sbatch_exp_launcher.bash
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 num_agents=4
-num_exp_per_env=2
+num_exp_per_env=1
 num_exp_env=1
 names=("Obs" "DMP_Obs" "Hist_Obs" "Force_only_DMP" "Force_only_Hist")
 echo "started"
 for env_idx in $(seq 0 $((num_exp_env - 1)))
 do
-    sbatch -J "${names[$exp_idx]}_test" -a 1-$num_exp_per_env exp_control/hpc_batch.bash $env_idx $num_agents 
+    #sbatch -J "${names[$exp_idx]}_test" -a 1-$num_exp_per_env exp_control/hpc_batch.bash $env_idx $num_agents 
+    sbatch -a 1-$num_exp_per_env exp_control/hpc_batch.bash $env_idx $num_agents 
 done
