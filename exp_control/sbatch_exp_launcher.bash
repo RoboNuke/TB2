ae4d772c2a7994b0cb28d97ee89e1f84fba024fe
Hunter <brownhun@oregonstate.edu>
2025-03-29 14:49:38 -0700

    Minibatch update

--- exp_control/sbatch_exp_launcher.bash
+++ exp_control/sbatch_exp_launcher.bash
@@ -7,6 +7,6 @@ names=("Obs" "DMP_Obs" "Hist_Obs" "Force_only_DMP" "Force_only_Hist")
 echo "started"
 for env_idx in $(seq 0 $((num_exp_env - 1)))
 do
-    sbatch -J "${names[$exp_idx]}_test" -a 1-$num_exp_per_env exp_control/hpc_batch.bash $env_idx $num_agents 
-    #sbatch -a 1-$num_exp_per_env exp_control/hpc_batch.bash $env_idx $num_agents 
+    #sbatch -J "${names[$exp_idx]}_test" -a 1-$num_exp_per_env exp_control/hpc_batch.bash $env_idx $num_agents 
+    sbatch -a 1-$num_exp_per_env exp_control/hpc_batch.bash $env_idx $num_agents 
 done
