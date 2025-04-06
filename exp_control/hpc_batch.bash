18df7e5135495ad2bc1e9aaf637241ad36d5395f
Hunter <brownhun@oregonstate.edu>
2025-04-06 03:12:59 -0700

    Ready for exp on hpc!

--- exp_control/hpc_batch.bash
+++ exp_control/hpc_batch.bash
@@ -8,7 +8,7 @@
 ##SBATCH -N 1                   # number of nodes (default 1)
 #SBATCH --gres=gpu:1            # number of GPUs to request (default 0)
 #SBATCH --mem=32G               # request 10 gigabytes memory (per node, default depends on node)
-#SBATCH -c 6                   # number of cores/threads per task (default 1)
+#SBATCH -c 8                   # number of cores/threads per task (default 1)
 #SBATCH -o ../outs/Param_%A_%a.out		# name of output file for this submission script
 #SBATCH -e ../outs/Param_%A_%a.err		# name of error file for this submission script
 # load any software environment module required for app (e.g. matlab, gcc, cuda)
