e867b363722931f79b34c70cbf8b164db63c3061
Hunter <brownhun@oregonstate.edu>
2025-04-11 23:07:49 -0700

    Preped exp, last_memory and no_curriculum

--- exp_control/sbatch_exp_launcher.bash
+++ exp_control/sbatch_exp_launcher.bash
@@ -2,7 +2,7 @@
 
 num_agents=4
 num_exp_per_env=2
-num_exp_env=2
+num_exp_env=1
 names=("Obs" "DMP_Obs" "Hist_Obs" "Force_only_DMP" "Force_only_Hist")
 
 for env_idx in $(seq 0 $((num_exp_env - 1)))
