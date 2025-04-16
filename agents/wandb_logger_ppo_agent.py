Fixed many problems with action scaling, wandb logging and general stupidity

agents/wandb_logger_ppo_agent.py
@@ -158,7 +158,7 @@ class WandbLoggerPPO(PPO):
         :type timesteps: int
         """
         if self.log_wandb:
-            prefix = "Eval " if eval else "Training "
+            prefix = "Eval_" if eval else "Training_"
 
             # handle cumulative rewards
             if len(self._track_rewards):
@@ -172,6 +172,10 @@ class WandbLoggerPPO(PPO):
                 self.tracking_data[prefix + "Episode / Total timesteps (max)"].append(np.max(track_timesteps))
                 self.tracking_data[prefix + "Episode / Total timesteps (min)"].append(np.min(track_timesteps))
                 self.tracking_data[prefix + "Episode / Total timesteps (mean)"].append(np.mean(track_timesteps))
+
+                for rew_type in self.rew_types:
+                    #print(f"final {rew_type}: { np.mean(self.rews['_track_'+rew_type]) }")
+                    self.track_data(prefix + "Reward / " + rew_type, np.mean(self.rews["_track_"+rew_type]))
 
             keep = {}
             for k, v in self.tracking_data.items():
@@ -195,15 +199,16 @@ class WandbLoggerPPO(PPO):
             tot = 0
             for name in self.totals:
                 self.data_manager.add_scalar(
-                    {prefix + " Termination / " + name: self.totals[name]}, 
+                    {prefix + "Termination / " + name: self.totals[name]}, 
                     timestep*self.num_envs
                 )
                 tot += 0 if 'engage' in name else self.totals[name]
                 self.totals[name] = 0
 
             timeouts = max(0, self.num_envs - tot)
+            self.curr_engaged[:] = False
             self.data_manager.add_scalar(
-                {prefix + " Termination / time_out" : timeouts}, 
+                {prefix + "Termination / time_out" : timeouts}, 
                 timestep*self.num_envs
             )
 
@@ -218,7 +223,11 @@ class WandbLoggerPPO(PPO):
         self._track_rewards.clear()
         self._track_timesteps.clear()
         self.tracking_data.clear()
+        for rew_type in self.rew_types:
+            self.rews[rew_type]*=0
+            self.rews["_track_"+rew_type].clear()
         
+        self.reset_tracking()
         if self.log_wandb:
             for k, v in keep.items():
                 self.tracking_data[k] = v
@@ -323,14 +332,14 @@ class WandbLoggerPPO(PPO):
             # goes through every sample in the memory (even if not filled), this way we set 
             # the actions to zero on termination, and then pass the fixed state in basically
             self.add_sample_to_memory(
-                states=states, 
-                actions=actions, 
-                rewards=rewards, 
-                next_states=next_states,
-                terminated=terminated,
-                truncated=truncated, 
-                log_prob=self._current_log_prob, 
-                values=values
+                states=states.clone(), 
+                actions=actions.clone(), 
+                rewards=rewards.clone(), 
+                next_states=next_states.clone(),
+                terminated=terminated.clone(),
+                truncated=truncated.clone(), 
+                log_prob=self._current_log_prob.clone(), 
+                values=values.clone()
             )
 
 
@@ -349,6 +358,12 @@ class WandbLoggerPPO(PPO):
                 self.once = {}
                 for name in infos['my_log_data']['once']:
                     self.once[name] = torch.zeros((self.num_envs,), dtype=torch.bool, device=self.device)
+                self.curr_engaged = torch.zeros((self.num_envs,), dtype=torch.bool, device=self.device)
+                self.rews = {}
+                self.rew_types = [name for name in infos['my_log_data']['step_rew']]
+                for name in self.rew_types:
+                    self.rews[name] = torch.zeros_like(rewards, dtype=torch.float32)
+                    self.rews["_track_"+name] = collections.deque(maxlen=1000)
 
             # handle reward 
             prefix = "Training "
@@ -377,17 +392,19 @@ class WandbLoggerPPO(PPO):
 
             if eval_mode:
                 self._cumulative_rewards += alive_mask * rewards
+
+                for rew_type in self.rew_types:
+                    #self.rews[rew_type] += alive_mask * infos['my_log_data']['step_rew'][rew_type]
+                    self.rews["_track_" + rew_type].extend(
+                        (alive_mask * self.rews[rew_type][:,0]).reshape(-1).tolist()
+                    )
+                    #self.rews[rew_type][finished_episodes] = 0
+
                 self._cumulative_timesteps[alive_mask] += 1
                 
-                mask_update = ~torch.logical_or(terminated, truncated)
-                new_terms = torch.logical_and(~mask_update, alive_mask)[:,0]
-                
-                for name in infos['my_log_data']["count_step"]:
-                    self.totals[name] += torch.sum(torch.logical_and(infos['my_log_data']['count_step'][name], new_terms))
-                
-                for name in infos['my_log_data']["once"]:
-                    self.once[name] = torch.logical_or(self.once[name], infos['my_log_data']['once'][name])
-
+                mask_update = ~torch.logical_or(terminated, truncated) # one that didn't term
+                finished_episodes = torch.logical_and(~mask_update, alive_mask)[:,0]
+                # term this step and alive overall
 
                 alive_mask *= mask_update
 
@@ -395,21 +412,40 @@ class WandbLoggerPPO(PPO):
                 self._cumulative_rewards.add_(rewards)
                 self._cumulative_timesteps.add_(1)
 
+                for rew_type in self.rew_types:
+                    rew = infos['my_log_data']['step_rew'][rew_type]
+                    #print(rew_type, rew.size(), self.rews[rew_type].size())
+                    self.rews[rew_type].add_(rew[:,None])
+
                 finished_episodes = (terminated + truncated).nonzero(as_tuple=False)
-                if finished_episodes.numel():
-                    # storage cumulative rewards and timesteps
-                    self._track_rewards.extend(self._cumulative_rewards[finished_episodes][:, 0].reshape(-1).tolist())
-                    self._track_timesteps.extend(self._cumulative_timesteps[finished_episodes][:, 0].reshape(-1).tolist())
 
-                    # reset the cumulative rewards and timesteps
-                    self._cumulative_rewards[finished_episodes] = 0
-                    self._cumulative_timesteps[finished_episodes] = 0
+            if finished_episodes.numel():
+                # storage cumulative rewards and timesteps
+                self._track_rewards.extend(self._cumulative_rewards[finished_episodes][:, 0].reshape(-1).tolist())
+                for rew_type in self.rew_types:
+                    #print(f'rew_type logging: {self.rews[rew_type][finished_episodes][:,0].reshape(-1).tolist()[0]}')
+                    self.rews["_track_" + rew_type].extend(self.rews[rew_type][finished_episodes][:,0].reshape(-1).tolist())
+                    self.rews[rew_type][finished_episodes] = 0
 
-                    for name in infos['my_log_data']["count_step"]:
-                        self.totals[name] += torch.sum(infos['my_log_data']['count_step'][name][finished_episodes])
+                self._track_timesteps.extend(self._cumulative_timesteps[finished_episodes][:, 0].reshape(-1).tolist())
 
-                for name in infos['my_log_data']["once"]:
-                    self.once[name] = torch.logical_or(self.once[name], infos['my_log_data']['once'][name])
+                # reset the cumulative rewards and timesteps
+                self._cumulative_rewards[finished_episodes] = 0
+                self._cumulative_timesteps[finished_episodes] = 0
+
+                for name in infos['my_log_data']["count_step"]:
+                    if 'engage' in name: 
+                        # engage is special because it doesn't have to be true at the end of rollout to count
+                        self.totals[name] += torch.sum(self.curr_engaged[finished_episodes].float())
+                        self.curr_engaged[finished_episodes] = False
+                    else:
+                        self.totals[name] += torch.sum(infos['my_log_data']['count_step'][name][finished_episodes].float())
+                    infos['my_log_data']['count_step'][name][finished_episodes] = False
+
+            for name in infos['my_log_data']["once"]:
+                self.once[name] = torch.logical_or(self.once[name], infos['my_log_data']['once'][name])
+                if 'engage' in name: # count step is the current status, once is the rollout status
+                    self.curr_engaged = torch.logical_or(self.curr_engaged, infos['my_log_data']['count_step'][name])
                     
         return alive_mask
     
@@ -418,11 +454,14 @@ class WandbLoggerPPO(PPO):
             return
         self._cumulative_rewards *= 0
         self._cumulative_timesteps *= 0
+        for rew_type in self.rew_types:
+            self.rews[rew_type] *= 0.0
+            self.rews["_track_"+rew_type].clear()
         
         for name in self.totals:
             self.totals[name] *= 0
-            self.once[name] *= 0
-
+            self.once[name][:] = False
+        self.curr_engaged[:] = False
         self._track_rewards.clear()
         self._track_timesteps.clear() 
 
@@ -440,24 +479,27 @@ class WandbLoggerPPO(PPO):
         super()._update(timestep, timesteps)
         # reset optimizer step
         self.resetAdamOptimizerTime(self.optimizer)
-        self.track_data(
-            "Layer Weight Norm / Output", 
-            torch.linalg.norm(self.value.critic.output[-1].weight, dim=None, ord=None).item()
-        )
-        self.track_data(
-            "Layer Weight Norm / Input", 
-            torch.linalg.norm(self.value.critic.input[0].weight, dim=None, ord=None).item()
-        )
+        
+        if self.cfg['track_layernorms']:
+            self.track_data(
+                "Layer Weight Norm / Output", 
+                torch.linalg.norm(self.value.critic.output[-2].weight, dim=None, ord=None).item()
+            )
+            self.track_data(
+                "Layer Weight Norm / Input", 
+                torch.linalg.norm(self.value.critic.input[0].weight, dim=None, ord=None).item()
+            )
 
-        for layer_idx, layer in enumerate(self.value.critic.layers):
-            self.track_data(
-                f"Layer Weight Norm / Layer {layer_idx}-1", 
-                torch.linalg.norm(layer.path[1].weight, dim=None, ord=None).item()
-            )
-            self.track_data(
-                f"Layer Weight Norm / Layer {layer_idx}-2", 
-                torch.linalg.norm(layer.path[3].weight, dim=None, ord=None).item()
-            )
+            for layer_idx, layer in enumerate(self.value.critic.layers):
+                self.track_data(
+                    f"Layer Weight Norm / Layer {layer_idx}-1", 
+                    torch.linalg.norm(layer.path[1].weight, dim=None, ord=None).item()
+                )
+                self.track_data(
+                    f"Layer Weight Norm / Layer {layer_idx}-2", 
+                    torch.linalg.norm(layer.path[3].weight, dim=None, ord=None).item()
+                )
+        
 
     def resetAdamOptimizerTime(self, opt):
         for p,v in opt.state_dict()['state'].items():

envs/factory/manager/mdp/events.py
@@ -288,30 +288,23 @@ def reset_master(
     else:
         # partial reset, reset indexes to fixed state
         # this shuffles the tensors
-        permutation = torch.randperm(env.num_envs)
-        for key in env.start_state:
-            #print(key)
-            for key2 in env.start_state[key]:
-                #print("\t", key2)
-                for key3 in env.start_state[key][key2]:
-                    #print("\t\t", key3)
-                    if key2 == 'robot' and key3 in ['root_pose', 'root_velocity']:
-                        continue        
-                    env.start_state[key][key2][key3][:] = env.start_state[key][key2][key3][permutation]
-
+        new_idxs = torch.randint(env.num_envs, (env_ids.size()[0],), device=env.device)
         for art_name in env.scene.articulations.keys():
-            pose = env.start_state['articulation'][art_name]['root_pose']
-            vel = env.start_state['articulation'][art_name]['root_velocity']
-            jnt_pos = env.start_state['articulation'][art_name]['joint_position']
-            jnt_vel = env.start_state['articulation'][art_name]['joint_velocity']
-            env.scene[art_name].write_root_pose_to_sim(pose[env_ids,:], env_ids=env_ids)
-            env.scene[art_name].write_root_velocity_to_sim(vel[env_ids,:], env_ids=env_ids)
-            env.scene[art_name].write_joint_state_to_sim(jnt_pos[env_ids,:], jnt_vel[env_ids,:], env_ids=env_ids)
+            #print("Articulation Name:", art_name)
+            pose = env.start_state['articulation'][art_name]['root_pose'][new_idxs,:]
+            pose[:,:3] = pose[:,:3] - env.scene.env_origins[new_idxs] + env.scene.env_origins[env_ids]
+            vel = env.start_state['articulation'][art_name]['root_velocity'][new_idxs,:]
+            jnt_pos = env.start_state['articulation'][art_name]['joint_position'][new_idxs,:]
+            jnt_vel = env.start_state['articulation'][art_name]['joint_velocity'][new_idxs,:]
+            env.scene[art_name].write_root_pose_to_sim(pose, env_ids=env_ids)
+            env.scene[art_name].write_root_velocity_to_sim(vel, env_ids=env_ids)
+            env.scene[art_name].write_joint_state_to_sim(jnt_pos, jnt_vel, env_ids=env_ids)
+            env.scene[art_name].reset(env_ids)
             #env.scene[art_name].data.default_root_state[env_ids,:7] = env.start_state['articulation'][art_name]['root_pose'][env_ids,:]
             #env.scene[art_name].data.default_root_state[:,7:] *= 0.0 
 
 
-        env.scene["held_asset"].reset()
+        #env.scene["held_asset"].reset()
         #set_assets_to_default_pose(env, torch.tensor(range(env.num_envs)))
         #set_franka_to_default_pose(env, torch.tensor(range(env.num_envs)))
         #print("partial reset")

envs/factory/manager/mdp/rewards.py
@@ -18,12 +18,17 @@ def force_check(
         raw = env.scene['force_torque_sensor'].data.net_forces_w_history
         mag = torch.linalg.norm(raw,dim=1)
         val = torch.max(raw, dim=1)
-        env.extras['my_log_data']['force_mag'] = val
-        return torch.where( val > threshold, 1.0, 0.0)
+        violated = torch.where( val > threshold, 1.0, 0.0)
     else:
-        mag = torch.linalg.norm(env.scene['force_torque_sensor'].data.net_forces_w, dim=1)
-        env.extras['my_log_data']['force_mag'] = mag
-        return torch.where(mag > threshold, 1.0, 0.0) 
+        val = torch.linalg.norm(env.scene['force_torque_sensor'].data.net_forces_w, dim=1)
+        violated = torch.where(mag > threshold, 1.0, 0.0) 
+
+    if 'my_log_data' not in env.extras:
+        env.extras['my_log_data'] = {}
+    env.extras['my_log_data']['force_mag'] = val
+    env.extras['my_log_data']['force_violations'] = violated
+
+    return violated
 
 def squashing_fn(x, a, b):
     return 1 / (torch.exp(a * x) + b + torch.exp(-a * x))
@@ -32,15 +37,25 @@ def squashing_fn(x, a, b):
 def keypoint_reward(
     env: ManagerBasedRLEnv,
     a: float = 100.0,
-    b: float = 0.0
+    b: float = 0.0,
+    rew_name: str = "keypoint_reward"
 ):
     compute_keypoint_value(env)#, dt=env.physics_dt))
-    return squashing_fn(env.keypoint_dist, a, b)
+    rew = squashing_fn(env.keypoint_dist, a, b)
+    if 'my_log_data' not in env.extras.keys():
+        env.extras['my_log_data'] = {}
+    if 'step_rew' not in env.extras['my_log_data']:
+        env.extras['my_log_data']['step_rew'] = {} 
+
+    env.extras['my_log_data']['step_rew'][rew_name] = rew
+    
+    return rew
 
 def currently_inrange(
     env: ManagerBasedRLEnv,
     success_threshold: float = 0.01,
-    check_rot: bool = False
+    check_rot: bool = False,
+    rew_name: str = "keypoint_reward"
 ):
     compute_keypoint_value(env)#, dt=env.physics_dt))
     curr_successes = torch.zeros((env.num_envs,), dtype=torch.bool, device=env.device)
@@ -64,22 +79,15 @@ def currently_inrange(
         curr_successes = torch.logical_and(curr_successes, is_rotated)
     
     if 'my_log_data' not in env.extras.keys():
-        env.extras['my_log_data'] = {
-            'success_once':torch.zeros_like(curr_successes),
-            'engaged_once':torch.zeros_like(curr_successes),
-            'count_step':{
-                'success':torch.zeros_like(curr_successes),
-                'engaged':torch.zeros_like(curr_successes)
-            },
-            'once':{
-                'success':torch.zeros_like(curr_successes),
-                'engaged':torch.zeros_like(curr_successes)
-            }
-        }
-
-    name = 'success' if success_threshold < 0.5 else 'engaged'
-    env.extras['my_log_data']['count_step'][name] = curr_successes
-    env.extras['my_log_data']['once'][name] = torch.logical_or(env.extras['my_log_data']['once'][name], curr_successes)
+        env.extras['my_log_data'] = {}
+    if 'count_step'  not in env.extras['my_log_data'].keys():
+        env.extras['my_log_data']['count_step'] = {}
+        env.extras['my_log_data']['once'] = {}
+    if rew_name not in env.extras['my_log_data']['count_step']:
+        env.extras['my_log_data']['count_step'][rew_name] = torch.zeros_like(curr_successes)
+        env.extras['my_log_data']['once'][rew_name] = torch.zeros_like(curr_successes)
+    env.extras['my_log_data']['count_step'][rew_name] = curr_successes
+    env.extras['my_log_data']['once'][rew_name] = torch.logical_or(env.extras['my_log_data']['once'][rew_name], curr_successes)
     
 
     return curr_successes

learning/single_agent_train.py
@@ -120,7 +120,7 @@ from agents.mp_agent import MPAgent
 import torch.multiprocessing as mp
 import copy
 from skrl.resources.schedulers.torch import KLAdaptiveLR
-
+import torch
 # seed for reproducibility
 #set_seed(args_cli.seed)  # e.g. `set_seed(42)` for fixed seed
 #if args_cli.seed == -1:
@@ -201,7 +201,9 @@ def main(
     print("Seed:", agent_cfg['seed'], args_cli.seed)
     #print(env_cfg)
     # random sample some parameters
-    #agent_cfg['agent']['learning_rate_scheduler'] = KLAdaptiveLR
+    if 'learning_rate_scheduler' in agent_cfg['agent'].keys():
+        # yaml doesn't read it as a class, but as a string idk
+        agent_cfg['agent']['learning_rate_scheduler'] = KLAdaptiveLR
     """
     import numpy as np
     agent_cfg['agent']['param_space'] = {}
@@ -412,6 +414,13 @@ def main(
                 device=device
             ) for i in range(args_cli.num_agents)
         ]
+        for i, agent in enumerate(agent_list):
+            agent.optimizer = torch.optim.Adam(
+                agent.policy.parameters(), 
+                lr=agent_cfgs[i]['agent']['learning_rate'],
+                betas=(0.999, 0.999)
+            )
+
     elif args_cli.learning_method == "sac":
         print("Have a sac actor")
         models['policy'] = BroActor(

models/SimBa.py
@@ -23,7 +23,7 @@ class SimBaLayer(nn.Module):
         return out + res
 
 class SimBaNet(nn.Module):
-    def __init__(self, n, in_size, out_size, latent_size, device):
+    def __init__(self, n, in_size, out_size, latent_size, device, tan_out=False):
         super().__init__()
         self.layers = []
         self.n = n
@@ -33,11 +33,17 @@ class SimBaNet(nn.Module):
 
         self.layers = nn.ModuleList([SimBaLayer(latent_size, device) for i in range(self.n)])
 
-        
-        self.output = nn.Sequential(
-            nn.LayerNorm(latent_size),
-            he_layer_init(nn.Linear(latent_size, out_size))
-        ).to(device)
+        if tan_out:
+            self.output = nn.Sequential(
+                nn.LayerNorm(latent_size),
+                he_layer_init(nn.Linear(latent_size, out_size)),
+                nn.Tanh()
+            ).to(device)
+        else:
+            self.output = nn.Sequential(
+                nn.LayerNorm(latent_size),
+                he_layer_init(nn.Linear(latent_size, out_size))
+            ).to(device)
 
     def forward(self, x):
         out =  self.input(x)
@@ -83,6 +89,7 @@ class SimBaAgent(GaussianMixin, DeterministicMixin, Model):
             in_size=in_size, 
             out_size=1, 
             latent_size=critic_latent, 
+            tan_out=False,
             device=device
         )
         
@@ -92,12 +99,13 @@ class SimBaAgent(GaussianMixin, DeterministicMixin, Model):
             in_size=in_size, 
             out_size=self.num_actions, 
             latent_size=actor_latent, 
-            device=device
+            device=device,
+            tan_out=True
         )
 
         he_layer_init(self.critic.output[-1], bias_const=critic_output_init_mean) # 2.5 is about average return for random policy w/curriculum
-        #with torch.no_grad():
-        #    self.actor_mean.output[-1].weight *= 0.01
+        with torch.no_grad():
+            self.actor_mean.output[-2].weight *= 0.01
 
         self.actor_logstd = nn.Parameter(
             torch.ones(1, self.num_actions) * math.log(act_init_std)
@@ -113,8 +121,11 @@ class SimBaAgent(GaussianMixin, DeterministicMixin, Model):
         if role == "policy":
             self._shared_output = self.feature_net(inputs)
             action_mean = self.actor_mean(self._shared_output)
+            #print("Action:", action_mean[0,:].T)
+            #print("Computed:", self.actor_logstd[0,0].item())
             return action_mean, self.actor_logstd.expand_as(action_mean), {}
         elif role == "value":
             shared_output = self.feature_net(inputs) if self._shared_output is None else self._shared_output
             self._shared_output = None
+            val = self.critic(shared_output)
             return self.critic(shared_output), {}